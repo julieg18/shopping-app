e1d02c8cd118734dd0da84e78b2954c65ec4902d
julieg18 <juliannagal18@gmail.com>
2020-07-21 06:15:20 -0500

    feat: setup css webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: { main: './src/scripts/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/shopping-app/',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
   },
   module: {
     rules: [
@@ -18,7 +23,23 @@ module.exports = {
         test: /\.html$/,
         loader: 'html-loader',
       },
+      {
+        test: /\.css$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin(),
+  ],
 };
